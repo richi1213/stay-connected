defa751f87d0f88d71eda9219ea8aba000736004
richi1213 <datonanava11@gmail.com>
2024-12-03 22:56:23 +0400

    added sending answer form

--- src/pages/question-page/views/question-page.tsx
+++ src/pages/question-page/views/question-page.tsx
@@ -6,49 +6,6 @@ import WriteAnswer from '@/pages/question-page/components/write-answer/write-ans
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
-// const questionWithAnswers = {
-//   question: {
-//     id: '28wdsay7dsasd',
-//     authorId: '82wewq7dsaghsa',
-//     title: 'How to use TypeScript with React?',
-//     author: 'Jane Doe',
-//     rating: 20,
-//     date: '24 Nov 2023',
-//     content:
-//       'I am trying to understand how to use TypeScript in a React project. Can anyone provide examples or resources?',
-//     tags: ['React', 'TypeScript'],
-//   },
-//   answers: [
-//     {
-//       id: 'answer1',
-//       username: 'nika123',
-//       rating: 15,
-//       date: '10 Nov 2024',
-//       content: 'h abjhsdas sda sdadiuasdai',
-//       likes: 2,
-//       isCorrect: false,
-//     },
-//     {
-//       id: 'answer2',
-//       username: 'diko123',
-//       rating: 30,
-//       date: '20 Nov 2024',
-//       content: 'h abjhsdas sda sdadiuasdai',
-//       likes: 1,
-//       isCorrect: false,
-//     },
-//     {
-//       id: 'answer3',
-//       rating: 0,
-//       username: 'nidshasba',
-//       date: '10 Nov 2024',
-//       content: 'h abjhsdas sda sdadiuasdai',
-//       likes: 10,
-//       isCorrect: false,
-//     },
-//   ],
-// };
-
 const QuestionPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   if (!id) {
@@ -82,12 +39,6 @@ const QuestionPage: React.FC = () => {
   if (isQuestionError) {
     return <div>Error fetching data</div>;
   }
-
-  // const {
-  //   question,
-  //   answers,
-  //   question: { authorId },
-  // } = questionWithAnswers;
 
   return (
     <div className='mb-8 mt-2 flex flex-col items-center'>
