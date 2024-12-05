d9dd018e4c3ac0540f1f906bbab809c5b49f3874
sopo <sofiko.imnaishvili@gmail.com>
2024-12-05 11:21:24 +0400

    pr

--- src/components/ui-blocks/question-cards/question-cards.tsx
+++ src/components/ui-blocks/question-cards/question-cards.tsx
@@ -17,46 +17,44 @@ interface QuestionCardProps {
 const QuestionCards: React.FC<PropsWithChildren<QuestionCardProps>> = ({
   questions,
 }) => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleCardClick = (id: string) => {
-    navigate(`/question/${id}`); 
+    navigate(`/question/${id}`);
   };
   return (
     <div className='flex flex-col gap-6'>
       {questions.map((question) => {
         const numberOfAnswers = question.answers?.length || 0;
         return (
-        
-            <Card onClick={() => handleCardClick(question.id)} >
-              <CardHeader>
-                <CardTitle className='text-lg'>{question.title}</CardTitle>
-                <CardDescription>{question.author.fullname} •</CardDescription>
-                {question.answers.some((answer) => answer.isCorrect) && (
-                  <div className='text-sm text-green-500'>Correct</div>
-                )}
-              </CardHeader>
-              <CardContent>
-                <p className='text-md text-muted-foreground'>
-                  {question.description}
-                </p>
-              </CardContent>
-              <CardFooter className='flex justify-between'>
-                <div className='flex gap-2'>
-                  {question.tag_names.map((tag) => {
-                    return (
-                      <Badge className='cursor-pointer' key={tag}>
-                        {tag}
-                      </Badge>
-                    );
-                  })}
-                </div>
-                <p className='text-sm text-muted-foreground'>
-                  Answers: {numberOfAnswers}
-                </p>
-              </CardFooter>
-            </Card>
-     
+          <Card onClick={() => handleCardClick(question.id)}>
+            <CardHeader>
+              <CardTitle className='text-lg'>{question.title}</CardTitle>
+              <CardDescription>{question.author.fullname} •</CardDescription>
+              {question.answers.some((answer) => answer.isCorrect) && (
+                <div className='text-sm text-green-500'>Correct</div>
+              )}
+            </CardHeader>
+            <CardContent>
+              <p className='text-md text-muted-foreground'>
+                {question.description}
+              </p>
+            </CardContent>
+            <CardFooter className='flex justify-between'>
+              <div className='flex gap-2'>
+                {question.tag_names.map((tag) => {
+                  return (
+                    <Badge className='cursor-pointer' key={tag}>
+                      {tag}
+                    </Badge>
+                  );
+                })}
+              </div>
+              <p className='text-sm text-muted-foreground'>
+                Answers: {numberOfAnswers}
+              </p>
+            </CardFooter>
+          </Card>
         );
       })}
     </div>
