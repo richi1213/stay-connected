0c37a04a9d60c358d23ec0aeeedaa462d8efe8d6
richi1213 <datonanava11@gmail.com>
2024-12-06 02:59:30 +0400

    added animations in SuccesMsg

--- src/pages/registration/views/success-msg.tsx
+++ src/pages/registration/views/success-msg.tsx
@@ -1,31 +1,49 @@
-// import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Check } from 'lucide-react';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 const SuccessMsg = () => {
   return (
-    <>
-      <div className='flex flex-col items-center space-y-2 p-4 text-foreground'>
-        <div>
-          <Check className='size-16 text-primary' />
-        </div>
+    <div className='flex flex-col items-center space-y-2 p-4 text-foreground'>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        <Check className='size-16 text-primary' />
+      </motion.div>
 
-        <h2 className='text-2xl font-semibold tracking-tight'>
-          Your account has been created!
-        </h2>
-        <p className='text-muted-foreground'>
-          Congratulations and welcome to the club
-        </p>
-        <div>
-          <Link to='/login'>
-            <Button className='mt-6 bg-primary px-8 font-medium' size='lg'>
-              Log In
-            </Button>
-          </Link>
-        </div>
-      </div>
-    </>
+      <motion.h2
+        className='text-2xl font-semibold tracking-tight'
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+      >
+        Your account has been created!
+      </motion.h2>
+
+      <motion.p
+        className='text-muted-foreground'
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.9 }}
+      >
+        Congratulations and welcome to the club
+      </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 1.1 }}
+      >
+        <Link to='/login'>
+          <Button className='mt-6 bg-primary px-8 font-medium' size='lg'>
+            Log In
+          </Button>
+        </Link>
+      </motion.div>
+    </div>
   );
 };
 
