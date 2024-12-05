e63591a0ae7c45451af7b11ffabdf6838cd3a7eb
richi1213 <datonanava11@gmail.com>
2024-12-05 23:29:45 +0400

    changed tailwind config file and little change in is accepted badge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss-animate';
+
 export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -53,5 +55,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [plugin],
 };
