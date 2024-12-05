227562531795658e9d4c5a64e544c890b90383d3
richi1213 <datonanava11@gmail.com>
2024-12-06 02:45:11 +0400

    refactoring and changing footer

--- src/components/ui-blocks/footer/footer.tsx
+++ src/components/ui-blocks/footer/footer.tsx
@@ -1,14 +1,20 @@
 import ScreenLg from '@/components/layout/page-containers/screen-lg';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className='border-border-soft border-t'>
+    <footer className='border-border-soft border-t'>
       <ScreenLg>
-        <div className='py-2 text-center'>
-          <p>Built by team 14. The source code is available on GitHub.</p>
+        <div className='flex flex-col items-center justify-center space-y-2'>
+          <div className='text-xl font-bold'>StayConnected</div>
+          <p className='text-sm text-muted-foreground'>
+            &copy; {currentYear} Team 14. All rights reserved.
+          </p>
         </div>
       </ScreenLg>
-    </div>
+    </footer>
   );
 };
+
 export default Footer;
