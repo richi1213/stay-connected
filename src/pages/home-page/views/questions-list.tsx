9aa1209a0b38d2689f8f11fc597154c4a09f9c8e
sopo <sofiko.imnaishvili@gmail.com>
2024-12-06 13:29:56 +0400

    add empty state for questions

--- src/pages/home-page/views/questions-list.tsx
+++ src/pages/home-page/views/questions-list.tsx
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import QuestionCards from '@/components/ui-blocks/question-cards/question-cards.tsx';
 import { Question } from '@/types/interfaces';
+import EmptyState from '@/components/ui-blocks/empty-state/empty-state';
 const QuestionsList = () => {
   const location = useLocation();
 
@@ -21,9 +22,16 @@ const QuestionsList = () => {
       {questions ? (
         <QuestionCards questions={questions} />
       ) : (
-        <div className='mt-8 flex w-full flex-col space-y-8 text-center font-semibold md:space-y-6 lg:space-y-8'>
-          Data Not Found
-        </div>
+        <EmptyState
+        title={
+        "There are no questions yet" 
+        }
+        buttonTitle='Ask a Question'
+        to='/createQuestion'
+      />
+        // <div className='mt-8 flex w-full flex-col space-y-8 text-center font-semibold md:space-y-6 lg:space-y-8'>
+        //   Data Not Found
+        // </div>
       )}
     </>
   );
