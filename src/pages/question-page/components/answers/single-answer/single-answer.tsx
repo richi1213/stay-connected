587507d5a6347b85fabb663e44588ed348ce30c5
sopo <sofiko.imnaishvili@gmail.com>
2024-12-07 11:30:09 +0400

    prettier

--- src/pages/question-page/components/answers/single-answer/single-answer.tsx
+++ src/pages/question-page/components/answers/single-answer/single-answer.tsx
@@ -130,7 +130,9 @@ const SingleAnswer: React.FC<ExtendedAnswer> = ({
               <p className='text-muted-foreground'>Rating: {author.rating}</p>
             </span> */}
             {isQuestionAuthor && (
-              <p className='text-sm text-muted-foreground'>Answer by the author</p>
+              <p className='text-sm text-muted-foreground'>
+                Answer by the author
+              </p>
               // <TooltipProvider>
               //   <Tooltip>
               //     <TooltipTrigger asChild>
