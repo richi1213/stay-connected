37e9ebdbd6ef3a26ec41b603d86168caaa7cbf49
sopo <sofiko.imnaishvili@gmail.com>
2024-12-07 14:15:56 +0400

    restore

--- src/pages/question-page/components/answers/single-answer/single-answer.tsx
+++ src/pages/question-page/components/answers/single-answer/single-answer.tsx
@@ -1,9 +1,15 @@
 import { Card, CardContent } from '@/components/ui/card';
 import { Toggle } from '@/components/ui/toggle';
-import { ThumbsUp } from 'lucide-react';
+import { ThumbsUp, Check, Dot, Speech, ListCheck } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/hooks/use-toast';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { ExtendedAnswer } from '@/pages/question-page/components/answers/answers.types';
 import { useAtomValue } from 'jotai';
 import { meAtom } from '@/store/auth';
@@ -13,8 +19,6 @@ import {
   markAnswerAsCorrect,
 } from '@/pages/question-page/api';
 import { useParams } from 'react-router-dom';
-import { Badge } from '@/components/ui/badge';
-import { useState } from 'react';
 
 const SingleAnswer: React.FC<ExtendedAnswer> = ({
   id: answerId,
@@ -29,7 +33,7 @@ const SingleAnswer: React.FC<ExtendedAnswer> = ({
   const { toast } = useToast();
   const me = useAtomValue(meAtom);
   const queryClient = useQueryClient();
-  const [badgeText, setBadgeText] = useState('Accept');
+
   const isQuestionAuthor = questionAuthorId === author.id;
   const curUserLiked = me ? authors_of_likes.includes(me.id) : false;
 
@@ -102,93 +106,68 @@ const SingleAnswer: React.FC<ExtendedAnswer> = ({
       queryClient.invalidateQueries(['answers', id]);
     },
   });
-  const handleMouseEnter = () => {
-    setBadgeText('Unaccept');
-  };
-
-  const handleMouseLeave = () => {
-    setBadgeText('Accepted');
-  };
 
   return (
-    <Card className='w-full border border-none border-neutral-200 bg-gray-50 text-foreground shadow-none'>
+    <Card className='w-full border-none bg-background text-foreground transition-all duration-300 ease-in-out hover:shadow-lg'>
       <CardContent className='space-y-2 p-4'>
-        <div className='flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between'>
-          <div className='flex items-center gap-2'>
-            <div className='flex items-center gap-2'>
-              <Avatar>
-                <AvatarImage src='' />
-                <AvatarFallback>{author.fullname.charAt(0)}</AvatarFallback>
-              </Avatar>
-              {isQuestionAuthor ? (
-                ''
-              ) : (
-                <div className='flex flex-col'>
-                  <p className='font-medium text-foreground'>
-                    {author.fullname}
-                  </p>
-                  <p className='text-sm text-muted-foreground'>
-                    Rating: {author.rating}
-                  </p>
-                </div>
-              )}
-            </div>
-            {/* <span className='font-medium'>{author.fullname}</span>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-1 sm:gap-3'>
+            <Avatar className='transition-transform duration-300 ease-in-out hover:scale-110'>
+              <AvatarImage src='' />
+              <AvatarFallback>{author.fullname.charAt(0)}</AvatarFallback>
+            </Avatar>
+            <span className='font-medium transition-colors duration-300 ease-in-out hover:text-primary'>
+              {author.fullname}
+            </span>
             <span className='flex items-center gap-0.5 text-sm text-primary'>
               <Dot className='hidden text-accent-foreground sm:block' />
-              <p className='text-muted-foreground'>Rating: {author.rating}</p>
-            </span> */}
-            {isQuestionAuthor && (
-              <p className='text-xs font-medium text-muted-foreground'>
-                ANSWERED BY THE AUTHOR
+              <p className='text-muted-foreground transition-opacity duration-300 ease-in-out hover:opacity-75'>
+                Rating: {author.rating}
               </p>
-              // <TooltipProvider>
-              //   <Tooltip>
-              //     <TooltipTrigger asChild>
-              //       <span className='ml-3'>
-              //         <Speech className='size-5 font-extrabold text-green-600' />
-              //       </span>
-              //     </TooltipTrigger>
-              //     <TooltipContent className='bg-primary text-primary-foreground'>
-              //       <p>Question Author</p>
-              //     </TooltipContent>
-              //   </Tooltip>
-              // </TooltipProvider>
+            </span>
+            {isQuestionAuthor && (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <span className='ml-3 transition-transform duration-300 ease-in-out hover:scale-110'>
+                      <Speech className='size-5 font-extrabold text-green-600' />
+                    </span>
+                  </TooltipTrigger>
+                  <TooltipContent className='bg-primary text-primary-foreground'>
+                    <p>Question Author</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             )}
           </div>
-          <div>
-            {me?.id === questionAuthorId &&
-              questionAuthorId !== author.id &&
-              (is_correct ? (
-                // <Button
-                //   variant='outline'
-                //   className='h-8 w-8 border-green-300 bg-green-100 px-0.5 text-green-800 hover:border-red-800 hover:bg-red-200 sm:h-auto sm:w-auto'
-                //   onClick={() => acceptAnswer()}
-                // >
-                //   <Check />
-                //   <span className='hidden sm:block'>Accepted</span>
-                // </Button>
-                <Badge
-                  variant='outline'
-                  className='w-auto border-green-300 bg-green-100 text-green-800 hover:cursor-pointer hover:border-red-300 hover:bg-red-100 hover:text-red-800'
+
+          {me?.id === questionAuthorId &&
+            questionAuthorId !== author.id &&
+            (is_correct ? (
+              <Button
+                variant='outline'
+                className='h-8 w-8 border-green-300 bg-green-100 px-1 text-green-800 transition-all duration-300 ease-in-out hover:border-red-800 hover:bg-red-200 sm:h-auto sm:w-auto'
+                onClick={() => acceptAnswer()}
+              >
+                <Check />
+                <span className='hidden sm:block'>Accepted</span>
+              </Button>
+            ) : (
+              <>
+                <Button
+                  variant='ghost'
+                  className='hidden text-primary transition-colors duration-300 ease-in-out hover:bg-primary hover:text-primary-foreground sm:block'
                   onClick={() => acceptAnswer()}
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
                 >
-                  {badgeText}
-                </Badge>
-              ) : (
-                <>
-                  <Button variant='secondary' onClick={() => acceptAnswer()}>
-                    Accept
-                  </Button>
-                  {/* <div className='sm:hidden'>
+                  Mark as correct
+                </Button>
+                <div className='sm:hidden'>
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger asChild>
                         <Button
                           variant='ghost'
-                          className='text-primary'
+                          className='text-primary transition-colors duration-300 ease-in-out hover:bg-primary hover:text-primary-foreground'
                           onClick={() => acceptAnswer()}
                         >
                           <ListCheck />
@@ -199,13 +178,14 @@ const SingleAnswer: React.FC<ExtendedAnswer> = ({
                       </TooltipContent>
                     </Tooltip>
                   </TooltipProvider>
-                </div> */}
-                </>
-              ))}
-          </div>
+                </div>
+              </>
+            ))}
         </div>
 
-        <div className='leading-relaxed'>{text}</div>
+        <div className='leading-relaxed transition-opacity duration-300 ease-in-out hover:opacity-90'>
+          {text}
+        </div>
 
         <div className='flex items-center gap-2 pt-2'>
           {me && (
@@ -213,10 +193,10 @@ const SingleAnswer: React.FC<ExtendedAnswer> = ({
               variant='outline'
               size='sm'
               onClick={() => toggleLike()}
-              className='data-[state=on]:bg-primary data-[state=on]:text-primary-foreground'
+              className='transition-all duration-300 ease-in-out data-[state=on]:bg-primary data-[state=on]:text-primary-foreground'
               data-state={curUserLiked ? 'on' : 'off'}
             >
-              <ThumbsUp className='mr-1 h-4 w-4 transition-all hover:text-primary' />
+              <ThumbsUp className='mr-1 h-4 w-4 transition-all duration-300 ease-in-out hover:text-primary' />
               <span className='text-sm'>{likes_count}</span>
             </Toggle>
           )}
