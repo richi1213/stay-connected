dc47228b412ea2ce003efad143d75acc33cb035d
sopo <sofiko.imnaishvili@gmail.com>
2024-12-07 11:16:18 +0400

    prettier

--- src/pages/question-page/components/answers/answers.tsx
+++ src/pages/question-page/components/answers/answers.tsx
@@ -6,18 +6,24 @@ const Answers: React.FC<AnswersProps> = ({ answers, questionAuthorId }) => {
     <div className='mt-6 space-y-6'>
       <h2 className='text-xl font-semibold'>Answers</h2>
       <div className='space-y-5'>
-        {answers.length > 0 ? answers.map((answer) => (
-          <SingleAnswer
-            key={answer.id}
-            id={answer.id}
-            text={answer.text}
-            likes_count={answer.likes_count}
-            authors_of_likes={answer.authors_of_likes}
-            is_correct={answer.is_correct}
-            author={answer.author}
-            questionAuthorId={questionAuthorId}
-          />
-        )) :  <p className="text-muted-foreground">This question isn't answered yet</p>}
+        {answers.length > 0 ? (
+          answers.map((answer) => (
+            <SingleAnswer
+              key={answer.id}
+              id={answer.id}
+              text={answer.text}
+              likes_count={answer.likes_count}
+              authors_of_likes={answer.authors_of_likes}
+              is_correct={answer.is_correct}
+              author={answer.author}
+              questionAuthorId={questionAuthorId}
+            />
+          ))
+        ) : (
+          <p className='text-muted-foreground'>
+            This question isn't answered yet
+          </p>
+        )}
       </div>
     </div>
   );
